c15283da1420b49bd945caa980304a2b62e83fb1
lpreimesberger <lpreimesberger@gmail.com>
2023-09-16 16:20:02 -0500

    ddd

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-# imports
 import math
 import struct
+
 import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib.animation as animation
+from matplotlib.lines import Line2D
 import pyaudio
-import ctypes
-time_read = []
-amplitude = []
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+DEVICE = 8
+RATE = 44100
+RECORD_SECONDS = 5
 
 
-# shows the sound waves
-def visualize(path: str):
-    plt.figure(1)
-    plt.title("Sound Wave")
-    plt.xlabel("Time")
-    plt.plot(time_read, amplitude)
-
+p = pyaudio.PyAudio()
+print("----------------------record device list---------------------")
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+print("USING ", DEVICE)
+RATE = int(p.get_device_info_by_index(DEVICE)['defaultSampleRate'])
+print(RATE)
+print("-------------------------------------------------------------")
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                input_device_index=DEVICE,
+                frames_per_buffer=CHUNK)
 
 
 
@@ -29,54 +47,58 @@ def rms(data_in):
     return math.sqrt( sum_squares / count )
 
 
-if __name__ == "__main__":
-    # gets the command line Value
-    for x in range(30):
-        time_read.append(x)
-        amplitude.append(0)
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    DEVICE = 8
-    RATE = 44100
-    RECORD_SECONDS = 5
-    p = pyaudio.PyAudio()
-    print("----------------------record device list---------------------")
-    info = p.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount')
-    for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
-    print("USING ", DEVICE)
-    RATE = int(p.get_device_info_by_index(DEVICE)['defaultSampleRate'])
-    print(RATE)
-    print("-------------------------------------------------------------")
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    input_device_index=DEVICE,
-                    frames_per_buffer=CHUNK)
 
-    print("* recording")
-    print(p.get_device_count())
-    frames = []
+class Scope:
+    def __init__(self, ax, maxt=2, dt=0.02):
+        self.ax = ax
+        self.dt = dt
+        self.maxt = maxt
+        self.tdata = [0]
+        self.ydata = [0]
+        self.line = Line2D(self.tdata, self.ydata)
+        self.ax.add_line(self.line)
+        self.ax.set_ylim(-.1, 1.1)
+        self.ax.set_xlim(0, self.maxt)
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    def update(self, y):
+        lastt = self.tdata[-1]
+        if lastt >= self.tdata[0] + self.maxt:  # reset the arrays
+            self.tdata = [self.tdata[-1]]
+            self.ydata = [self.ydata[-1]]
+            self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
+            self.ax.figure.canvas.draw()
+
+        # This slightly more complex calculation avoids floating-point issues
+        # from just repeatedly adding `self.dt` to the previous value.
+        t = self.tdata[0] + len(self.tdata) * self.dt
+
+        self.tdata.append(t)
+        self.ydata.append(y)
+        self.line.set_data(self.tdata, self.ydata)
+        return self.line,
+
+
+def emitter(p=0.1):
+    """Return a random value in [0, 1) with probability p, else 0."""
+    while True:
         data = stream.read(CHUNK, exception_on_overflow=False)
-        frames.append(data)
         power = rms(data)
-        for shift in range(len(amplitude)):
-            if shift == 0:
-                continue
-            amplitude[shift-1] = amplitude[shift]
-        amplitude[len(amplitude)-1] = power
+        yield power
 
 
 
-    print("* done recording")
 
+fig, ax = plt.subplots()
+scope = Scope(ax)
+
+try:
+    # pass a generator in "emitter" to produce data for the update func
+    ani = animation.FuncAnimation(fig, scope.update, emitter, interval=50,
+                                  blit=True, save_count=100)
+
+    plt.show()
+finally:
+    print("closing alsa")
     stream.stop_stream()
     stream.close()
     p.terminate()
-    visualize("")
